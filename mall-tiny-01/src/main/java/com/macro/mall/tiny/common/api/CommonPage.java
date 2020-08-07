0a3b876aaa49303c5701cf5ab164138b89674059
lixingkai <lixingkai2@126.com>
2020-08-08 05:49:18 +0800

    新增模块

--- mall-tiny-01/src/main/java/com/macro/mall/tiny/common/api/CommonPage.java
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/common/api/CommonPage.java
@@ -7,23 +7,23 @@ import java.util.List;
  * Created by macro on 2019/4/19.
  */
 public class CommonPage<T> {
-    private Integer pageNum;
-    private Integer pageSize;
-    private Integer totalPage;
-    private Long total;
-    private List<T> list;
+    private Integer pageNum;    //当前页
+    private Integer pageSize;   //每页的数量
+    private Integer totalPage;  //总页数
+    private Long total;         //总记录数
+    private List<T> list;       //结果集
 
     /**
      * 将PageHelper分页后的list转为分页信息
      */
     public static <T> CommonPage<T> restPage(List<T> list) {
         CommonPage<T> result = new CommonPage<T>();
-        PageInfo<T> pageInfo = new PageInfo<T>(list);
-        result.setTotalPage(pageInfo.getPages());
-        result.setPageNum(pageInfo.getPageNum());
-        result.setPageSize(pageInfo.getPageSize());
-        result.setTotal(pageInfo.getTotal());
-        result.setList(pageInfo.getList());
+        PageInfo<T> pageInfo = new PageInfo<T>(list);   //拿结果集来分页
+        result.setTotalPage(pageInfo.getPages());       //设置总页数
+        result.setPageNum(pageInfo.getPageNum());       //设置当前页
+        result.setPageSize(pageInfo.getPageSize());     //每页的数量
+        result.setTotal(pageInfo.getTotal());           //总记录数
+        result.setList(pageInfo.getList());             //结果集
         return result;
     }
 
